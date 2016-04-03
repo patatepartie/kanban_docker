6fda9e4851f8d314f896ee5bb97a1cf49b267aa1
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-04-04 08:22:44 +0900

    Hot css reloading

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,15 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
   }
 };
 
