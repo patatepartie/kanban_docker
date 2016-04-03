17b27d1a6fcc754cd29f863ec29eb1f1daf5e05a
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-04-04 08:17:12 +0900

    HMR for dev server
    Also use the correct default for the host (`0.0.0.0`).
    Running dev is now as simple as:
    ```sh
    docker/run -p 8080:8080 kanban npm start
    ```

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
     app: PATHS.app
   },
@@ -14,3 +17,26 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      stats: 'errors-only',
+      host: process.env.HOST || '0.0.0.0',
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
