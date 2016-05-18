d18e50a3c7a96231e82c0f19b836f59c0e5d7bba
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-05-18 22:20:42 +0900

    Create babel caches outside of workdir

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const common = {
       },
       {
         test: /\.jsx?$/,
-        loaders: ['babel?cacheDirectory'],
+        loaders: ['babel?cacheDirectory=/tmp'],
         include: PATHS.app
       }
     ]
