58b3d7c24a354105c3380816d0fe2767462b3cc2
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-05-18 23:17:04 +0900

    Extract Notes and Note components out of App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,8 @@
 import uuid from 'node-uuid';
 import React from 'react';
 
+import Notes from './Notes.jsx'
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -30,9 +32,7 @@ export default class App extends React.Component {
       <div>
         <button onClick={this.addNote}>+</button>
 
-        <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>
-        )}</ul>
+        <Notes notes={notes} />
       </div>
     );
   }
