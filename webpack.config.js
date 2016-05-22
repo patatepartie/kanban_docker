6ae7368c8b1525db4f1225703e738e91d068b3bc
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-05-23 08:25:12 +0900

    Suffix bundles with their hashes for cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,11 @@ if (TARGET === 'build') {
         return v !== 'alt-utils';
       })
     },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: 'chunkhash.js'
+    },
     plugins: [
       new webpack.optimize.CommonsChunkPlugin({
         names: ['vendor', 'manifest']
