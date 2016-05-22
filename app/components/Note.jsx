3b4744707a7ce27bb027ea1e4ce2da0ba5cc3e36
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-05-23 07:45:54 +0900

    Make dragged Note invisible (but still taking space) when dragging

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -23,19 +23,20 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
 }))
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
   connectDropTarget: connect.dropTarget()
 }))
 export default class Note extends React.Component {
   render() {
-    const {connectDragSource, connectDropTarget, ...props} = this.props;
+    const {connectDragSource, connectDropTarget, isDragging, ...props} = this.props;
 
     return connectDragSource(
       connectDropTarget(
-        <li {...props}>{props.children}</li>
+        <li style={{opacity: isDragging ? 0 : 1}} {...props}>{props.children}</li>
       )
     );
   }
