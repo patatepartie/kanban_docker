42c59983d1530ee1ca5cfb42a9b7832b1c7c3cb6
Cyril Ledru <cyril.ledru@kvh.co.jp>
2016-05-22 22:41:46 +0900

    Prepare Notes to be dragged

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Editable from './Editable.jsx';
+import Note from './Note.jsx';
 
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map(note =>
-      <li className="note" key={note.id}>
+      <Note className="note" id={note.id} key={note.id}>
         <Editable
           editing={note.editing}
           value={note.task}
@@ -13,7 +14,7 @@ export default ({notes, onValueClick, onEdit, onDelete}) => {
           onValueClick={onValueClick.bind(null, note.id)}
           onDelete={onDelete.bind(null, note.id)}
         />
-      </li>
+      </Note>
     )}</ul>
   );
 }
